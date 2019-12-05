9dd67ea5fddee9670f01c0546a352d5922a05bb5
Logan Negley <logannegley@gmail.com>
2019-12-04 20:16:37 -0600

    created route for home contact and about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path='/' componet={Home}/>
+    <Route pathe='/about' componet={About}/>
+    <Route path = '/contact' component ={Contact}/>
   </div>
 );
 
