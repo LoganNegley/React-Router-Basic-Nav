a5cf242cc23804d19634b98edbfc14c06c09999d
Logan Negley <logannegley@gmail.com>
2020-01-14 20:46:05 -0600

    changed all a tags to Links and imported Link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => (
     <Navigation />
 
 
-    <Route path='/' component='Home'/>
+    <Route exact path='/' component='Home'/>
     <Route path='/about' component='About'/>
     <Route path='/contact' component='Contact'/>
   </div>
